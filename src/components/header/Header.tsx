561268826283cfd9347bbf3757052f466a236d0f
raazymuhd1 <muhammadrazi51@gmail.com>
2025-03-18 10:06:08 +0700

    handled an active link

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
         const clientHeight = window.scrollY
         console.log(clientHeight)
      }
-  }, [window.scrollY])
+  }, [typeof window != "undefined" && window.scrollY])
 
   return (
     <header className="w-full sticky bg-[rgba(9,9,11,255)] top-0 h-[70px] py-[15px] border-b-[1px] border-b-[#202021] z-[2]">
--- src/components/header/Links.tsx
+++ src/components/header/Links.tsx
@@ -3,13 +3,19 @@ import { navbarLists } from '@/constants'
 import { CustomButton } from "@/components"
 import Link from 'next/link'
 import { LuWallet } from "react-icons/lu";
+import { useSelectedLayoutSegment, usePathname  } from 'next/navigation'
 
 const Links = () => {
+    const segment = useSelectedLayoutSegment();
+    const path = usePathname()
+
+    console.log(path.split('/')[1])
+
   return (
     <ul className="md:flex items-center gap-[40px] hidden ">
             { navbarLists.map(list => (
                 <Link
-                  className="cursor-pointer "
+                  className={`cursor-pointer ${(path.split('/')[1].length == 0 ? "home" : path.split('/')[1]) == list.title.toLowerCase() && "border-b-[2px] border-[#2caec5]"}`}
                   key={list.id} href={list.url}> 
                   { list.title } 
                 </Link>
