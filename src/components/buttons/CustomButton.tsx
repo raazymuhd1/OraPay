a47af43e425bf8b871236383b80dde331e44c961
raazymuhd1 <muhammadrazi51@gmail.com>
2025-03-18 09:46:13 +0700

    added a shadow light on hero section

--- src/components/buttons/CustomButton.tsx
+++ src/components/buttons/CustomButton.tsx
@@ -1,20 +1,26 @@
-import { ReactNode } from 'react'
+import { ReactNode, Ref, forwardRef, MouseEventHandler } from 'react'
 
 interface IProps {
     children: ReactNode,
     onClick?: () => void,
     disabled?: boolean,
     style?: string,
+    mouseEvent?: MouseEventHandler<HTMLButtonElement>;
+    mouseLeave?: MouseEventHandler<HTMLButtonElement>;
 }
+// forwardRef will expose the btn dom to the parent component
+const CustomButton = forwardRef(({ children, onClick, disabled, style, mouseEvent, mouseLeave}: IProps, ref: Ref<HTMLButtonElement>) => {
 
-const CustomButton = ({ children, onClick, disabled, style }: IProps) => {
   return (
     <button 
+        ref={ref}
         onClick={onClick}
+        onMouseOver={mouseEvent}
+        onMouseLeave={mouseLeave}
         className={`flex items-center justify-center gap-[10px] text-white font-semibold rounded-[10px] cursor-pointer py-[6px] px-[10px] ${style} `}>
         { children }
     </button>
   )
-}
+})
 
 export default CustomButton
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,13 +1,28 @@
 "use client"
-import React from 'react'
+import {useRef} from 'react'
 import { MoveRight } from "lucide-react"
 import { CustomButton } from "@/components"
 import Link from 'next/link'
 
 const Hero = () => {
+        const btnRef = useRef<HTMLButtonElement>(null)
+
+        const handleOnMouseEnter = () => {
+            console.log(btnRef.current)
+            btnRef.current?.classList.add("btn-shadow")
+        }
+
+        const handleOnMouseLeave = () => {
+            console.log(btnRef.current)
+            btnRef.current?.classList.remove("btn-shadow")
+        }
+
   return (
-    <section className="w-full h-[700px] relative top-0">
-        <div className="w-[30%] h-full items-center justify-center flex flex-col gap-[10px] mx-auto">
+    <section className="w-full h-[700px] mx-auto flex items-center gap-[10px]">
+        
+        <div className=" h-[200px] rounded-[50%] shadowing-left bg-[rgba(82,122,254,255)]" />
+
+        <div className="w-[50%] h-full items-center justify-center flex flex-col gap-[10px] mx-auto">
             <h4 className="rounded-[30px] text-[.7vmax] text-[#11afb8] bg-[rgba(22,22,23,255)] border-[1px] border-[#7f7f80] py-[3px] px-[8px]"> Introducing: The Future of Payments </h4>
             <h2 className="text-[#0ff0fc] font-bold text-[2.9vmax]">
                 Buy
@@ -18,13 +33,19 @@ const Hero = () => {
 
             <div className="flex items-center gap-[20px] mt-[20px]">
                 <Link href="/dashboard">
-                    <CustomButton disabled={false} style={`bg-gradient h-[50px] w-[150px]`} onClick={() => {}} > Start Now <MoveRight className="" /> </CustomButton>
+                    <CustomButton 
+                        ref={btnRef}
+                        mouseEvent={handleOnMouseEnter}
+                        mouseLeave={handleOnMouseLeave}
+                        disabled={false} style={`bg-gradient h-[50px] w-[150px] hover:translate-y-[-5px] transition-all duration-500`} onClick={() => {}} > Start Now <MoveRight className="" /> </CustomButton>
                 </Link>
                 <Link href="/">
                     <CustomButton disabled={false} style={`h-[50px] bg-[rgba(22,22,23,255)] w-[150px]`} onClick={() => {}} > Learn More </CustomButton>
                 </Link>
             </div>
         </div>
+
+        <div className="h-[200px] rounded-[50%] shadowing-right bg-[rgba(82,122,254,255)]" />
 
     </section>
   ) 
