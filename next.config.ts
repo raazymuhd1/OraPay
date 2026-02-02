cd9e8668a348c617363d64b4f255878b0cbef67f
raazymuhd1 <muhammadrazi51@gmail.com>
2026-02-03 05:22:39 +0700

    setup for turbopack on next.config

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    turbopack: {}
 };
 // civic auth config
 const withCivicAuth = createCivicAuthPlugin({
   clientId: process.env.CIVIC_CLIENT_ID as string, // Civic's client ID,
-  basePath: "/"
+  basePath: "/",
 });
 
 export default withCivicAuth(nextConfig)
